0f96eb4874f6ff376ceca219219f2cd9c473a77e
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2025-07-13 01:41:39 +0300

    Refactor Context

--- src/Handler/Context.php
+++ src/Handler/Context.php
@@ -9,7 +9,15 @@ final class Context
     /**
      * @var array<class-string, object>
      */
-    private array $values = [];
+    private array $objects = [];
+
+    /**
+     * @param class-string $class
+     */
+    public function has(string $class): bool
+    {
+        return isset($this->objects[$class]);
+    }
 
     /**
      * @template T of object
@@ -19,15 +27,7 @@ final class Context
     public function get(string $class, ?string $notFoundMessage = null): object
     {
         /** @var T */
-        return $this->values[$class] ?? throw new \LogicException($notFoundMessage ?? "Value registered as `{$class}` not found");
-    }
-
-    /**
-     * @param class-string $class
-     */
-    public function has(string $class): bool
-    {
-        return isset($this->values[$class]);
+        return $this->objects[$class] ?? throw new \LogicException($notFoundMessage ?? "Value registered as `{$class}` not found");
     }
 
     /**
@@ -38,7 +38,7 @@ final class Context
     public function find(string $class): ?object
     {
         /** @var ?T */
-        return $this->values[$class] ?? null;
+        return $this->objects[$class] ?? null;
     }
 
     /**
@@ -50,7 +50,7 @@ final class Context
 
         foreach ($as ?: [$value::class] as $class) {
             \assert($value instanceof $class);
-            $context->values[$class] = $value;
+            $context->objects[$class] = $value;
         }
 
         return $context;
