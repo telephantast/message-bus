18786acce8906af71160c50927142f83c3f36ef6
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-07-09 06:54:38 +0400

    Add generic to Envelope

--- src/MessageBus/Envelope.php
+++ src/MessageBus/Envelope.php
@@ -6,9 +6,14 @@ namespace Thesis\MessageBus;
 
 /**
  * @api
+ *
+ * @template T of object = object
  */
 final readonly class Envelope
 {
+    /**
+     * @param T $payload
+     */
     public function __construct(
         public object $payload,
         public Metadata $metadata,
